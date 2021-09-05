cccf7a29bf3d2fd6131edf8164a33d688574d69f
Aman <aman.k9250@gmail.com>
2021-09-05 20:48:12 +0530

    Added About Section and sticky header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,14 @@
-
+// sticky navbar
+$(window).scroll(function(){
+    if($(this).scrollTop() > 100){
+        $('nav').addClass('sticky')
+        $('.menu-icon').addClass('sticky')
+    } else{
+        $('nav').removeClass('sticky')
+        $('.menu-icon').removeClass('sticky')
+    }
+});
+// code for responsive menu
 const hamburger = document.querySelector(".menu-icon");
 const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () =>{
